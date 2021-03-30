cc6e41d7e7f928cdbda72d61548993593c1b34c1
devopesj <ope.junaid@gmail.com>
2021-03-30 21:04:57 +0100

    new template id

--- instance-launch/create-instance.sh
+++ instance-launch/create-instance.sh
@@ -24,6 +24,6 @@ envsubst <record.json >/tmp/${component}.json
 
 aws route53 change-resource-record-sets --hosted-zone-id Z01740343DEHVU707V4GH --change-batch file:///tmp/${component}.json
 
-sed -i -e "${component}/ d" ../inventory
+sed -i -e "/${component}/ d" ../inventory
 PUBLIC_IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
 echo "${PUBLIC_IPADDRESS} APP=${component}" >>../inventory
