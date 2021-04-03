4825ca90b4b83f6ed173097c32ce69452f08f036
devopesj <ope.junaid@gmail.com>
2021-04-03 22:54:55 +0100

    inv

--- instance-launch/create-instance.sh
+++ instance-launch/create-instance.sh
@@ -24,6 +24,7 @@ envsubst <record.json >/tmp/${component}.json
 
 aws route53 change-resource-record-sets --hosted-zone-id Z01740343DEHVU707V4GH --change-batch file:///tmp/${component}.json
 
+### This is for inventory file
 sed -i -e "/${component}/ d" ../inventory
 PUBLIC_IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
 echo "${PUBLIC_IPADDRESS} APP=${component}" >>../inventory
