e5ac66251f82978165664ad22159dbbd56a56cd4
devopesj <ope.junaid@gmail.com>
2021-04-13 16:00:23 +0100

    create all instances at once

--- instance-launch/create-instance.sh
+++ instance-launch/create-instance.sh
@@ -4,6 +4,26 @@ sudo yum install gettext -y &>/dev/null
 
 component=$1
 
+if [ "$1{component}" == "all"]; then
+
+  for component in frontend mongodb catalogue redis user cart mysql shipping rabbitmq payment; do
+    echo Creating ${component} server
+    STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].State.Name' --output text)
+    if [ "$STATE" != "running" ]; then
+      aws ec2 run-instances --launch-template LaunchTemplateId=lt-097d20195887becd4 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" &>/tmp/{component}.log
+      sleep 5
+    fi
+    IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+    export component
+    export IPADDRESS
+    envsubst <record.json >/tmp/${component}.json
+    sed -i -e "/${component}/ d" ../inventory
+    PUBLIC_IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
+    echo "${PUBLIC_IPADDRESS} APP=${component}" >>../inventory
+  done
+  exit
+fi
+
 if [ -z "${component}" ]; then
   echo "Need a Input of Component Name"
   exit 1
@@ -12,7 +32,7 @@ fi
 STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].State.Name' --output text)
 
 if [ "$STATE" != "running" ]; then
-  aws ec2 run-instances  --launch-template LaunchTemplateId=lt-097d20195887becd4 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" | jq .
+  aws ec2 run-instances --launch-template LaunchTemplateId=lt-097d20195887becd4 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" | jq .
   sleep 15
 fi
 
